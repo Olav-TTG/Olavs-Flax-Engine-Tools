daa136839f30877c24e417886e7260c43b0a3b21
olavaketun <olav.aketun@outlook.com>
2026-02-13 14:22:50 +0100

    ADD String and Math tools
    Added first iteration of string and math tools.

---
+++ Utilities/MathTools.cs
@@ -0,0 +1,11 @@
+﻿using FlaxEngine;
+
+namespace Utilities;
+
+public class MathTools
+{
+    public static float Magnitude(Vector3 vec3) //Unity-like vector3 magnitude
+    {
+        return float.Sqrt((vec3.X * vec3.X) + (vec3.Y * vec3.Y) + (vec3.Z * vec3.Z));
+    }
+}
---
+++ Utilities/StringTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Utilities;
+
+public class StringTools
+{
+    public static float StringToFloat(string self) //Removes all text from a string, leaving only numbers and separators.
+    {
+        string stripAlphabet = new string(self.Where(c=>(Char.IsDigit(c) || c=='.'|| c==',')).ToArray());
+        float stringFloat;
+        float.TryParse(stripAlphabet, out stringFloat);
+        return stringFloat;
+    }
+}
