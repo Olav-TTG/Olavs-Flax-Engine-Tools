1fd4286403575d38865c604055e69a600849262d
olavaketun <olav.aketun@outlook.com>
2026-02-14 11:23:34 +0100

    UPDATE mathtools
    Added ClampMagnitude similar to Unitys clamp magnitude

--- Utilities/MathTools.cs
+++ Utilities/MathTools.cs
@@ -8,4 +8,23 @@ public class MathTools
     {
         return float.Sqrt((vec3.X * vec3.X) + (vec3.Y * vec3.Y) + (vec3.Z * vec3.Z));
     }
+
+    public static Vector3 ClampMagnitude(Vector3 vec3, float maxLength)
+    {
+        Vector3 result;
+        if (Magnitude(vec3) > maxLength * maxLength)
+        {
+            result = vec3.Normalized * maxLength;
+        }
+        else
+        {
+            result = vec3;
+        }
+        return result;
+    }
+
+    public Vector3 Normalized
+    {
+        get { return Vector3.Normalize(Normalized); }
+    }
 }
