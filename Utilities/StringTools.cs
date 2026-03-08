fc37154de8406dac4ce6737dbd2f244ff144c529
olavaketun <olav.aketun@outlook.com>
2026-03-08 22:07:25 +0100

    UPDATE StringTools
    Added string to int version

--- Utilities/StringTools.cs
+++ Utilities/StringTools.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
 namespace Utilities;
 
 public class StringTools
 {
-    public static float StringToFloat(string self) //Removes all text from a string, leaving only numbers and separators.
+    //Removes all text from a string, leaving only numbers and separators.
+    public static float StringToFloat(string self)
     {
+        CultureInfo cultureInfo = new CultureInfo("en-US", false);
         string stripAlphabet = new string(self.Where(c=>(Char.IsDigit(c) || c=='.'|| c==',')).ToArray());
-        float stringFloat;
-        float.TryParse(stripAlphabet, out stringFloat);
+        //stripAlphabet.Replace(",", ".");
+        //string.Format(cultureInfo, stripAlphabet);
+        float.TryParse(stripAlphabet.Replace(",", "."), cultureInfo, out float stringFloat);
         return stringFloat;
+    }
+    public static int StringToInt(string self)
+    {
+        CultureInfo cultureInfo = new CultureInfo("en-US", false);
+        string stripAlphabet = new string(self.Where(c=>(Char.IsDigit(c) || c=='.'|| c==',')).ToArray());
+        //stripAlphabet.Replace(",", ".");
+        //string.Format(cultureInfo, stripAlphabet);
+        int.TryParse(stripAlphabet.Replace(",", "."), cultureInfo, out int stringInt);
+        return stringInt;
     }
 }
